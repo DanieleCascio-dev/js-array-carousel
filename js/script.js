582dc09735cc598f815c2934dbdc25fadcb1b867
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-24 17:23:42 +0200

    add: infinite cicle on "next"

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //Dichiarazioni variabili
-
 const items = document.querySelector(".items");
 let imgMess = "";
 
@@ -10,7 +9,6 @@ const arrayImg = [
   "img/04.jpg",
   "img/05.jpg",
 ];
-
 // console.log(arrayImg);
 
 for (let i = 0; i < arrayImg.length; i++) {
@@ -18,34 +16,45 @@ for (let i = 0; i < arrayImg.length; i++) {
   imgMess += ` <div class="item">
   <img src="${curImg}" alt="" />
 </div> `;
-  /* console.log(imgMess); */
+  // console.log(imgMess);
 }
-console.log(imgMess);
+
+//Put the img into html
 items.innerHTML += imgMess;
 
 //Array with all item in html
 const allImg = document.querySelectorAll(".item");
 console.log(allImg);
 
-//Set the first img to display block
-allImg[0].classList.add("active");
+//set a variable for last postion of allImg
+const lastPos = allImg.length - 1;
 
 //set a variable for index
 let indexImg = 0;
 
+//Set the first img to display block
+allImg[0].classList.add("active");
+
 //Event listener and for iterations to "next" div
 document.querySelector(".next").addEventListener("click", function () {
-  allImg[indexImg].classList.remove("active");
-  console.log(allImg[indexImg]);
-  indexImg++;
-  allImg[indexImg].classList.add("active");
+  if (indexImg >= lastPos) {
+    allImg[indexImg].classList.remove("active");
+    indexImg = 0;
+    allImg[indexImg].classList.add("active");
+    console.log(allImg[indexImg]);
+  } else {
+    allImg[indexImg].classList.remove("active");
+    console.log(allImg[indexImg]);
+    indexImg++;
+    allImg[indexImg].classList.add("active");
+  }
 });
 
 //Event listener and for iterations to "prev" div
 document.querySelector(".prev").addEventListener("click", function () {
   if (indexImg <= 0) {
     allImg[indexImg].classList.remove("active");
-    indexImg = allImg.length - 1;
+    indexImg = lastPos;
     allImg[indexImg].classList.add("active");
     console.log(allImg[indexImg]);
   } else {
